c65ba15ce0884c314a4b9b0e43995db482963e50
Kenny Arehart <kennyarehart@gmail.com>
2019-11-19 17:47:25 -0800

    added header to the clients section

--- src/components/clients/clients-view.js
+++ src/components/clients/clients-view.js
@@ -5,6 +5,10 @@ import './clients.scss'
 function Clients() {
 	return (
 		<div className="clients-container">
+			<h4>
+				I'm proud to have collaborated with
+				<br /> some awesome brands / companies:
+			</h4>
 			<div className="clients-list">
 				{json.list.map((item, i) => {
 					return (
